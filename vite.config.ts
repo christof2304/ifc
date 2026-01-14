2e5b0246833a6742ae079b2f017211b0bb84ef18
christof2304 <christof2304@gmail.com>
2026-01-14 23:06:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Für GitHub Pages: Repository-Name als base setzen
   // Ändere 'ifc-viewer' zu deinem Repository-Namen!
-  base: '/ifc-viewer/',
+  base: '/ifc/',
   
   build: {
     target: 'esnext',
